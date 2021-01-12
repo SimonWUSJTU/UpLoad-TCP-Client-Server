bef11efa90122254b35dd9ca2f6e0dfae23e527d
SimonWUSJTU <72699728+SimonWUSJTU@users.noreply.github.com>
2021-01-12 22:21:41 +0800

    Create TCPClient.java

---
+++ TCPClient.java
@@ -0,0 +1,36 @@
+package BlackMa;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class TCPClient {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis=new FileInputStream("D:\\workthings\\a.jpg");
+
+        Socket socket=new Socket("127.0.0.1",8888);
+
+        OutputStream ops=socket.getOutputStream();
+
+        int len=0;
+        byte[] bytes=new byte[1024];
+
+        while((len=fis.read(bytes))!=-1){
+        ops.write(bytes,0,len);
+        }
+
+        socket.shutdownOutput();
+        System.out.println("Yourenma ");
+       InputStream is=socket.getInputStream();
+
+        while((len=is.read(bytes))!=-1){
+            System.out.println(new String(bytes,0,len));
+        }
+
+        fis.close();
+        socket.close();
+
+    }
+}
